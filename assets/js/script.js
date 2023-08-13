23d436d0252fa0edb3869fe0d24ab35616ee300b
sarebok <bortonae@hotmail.com>
2023-08-13 20:06:30 -0300

    delete consolelogs, ordering the code a little bit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
 //aqui empieza el js del trabajo 1
 const imgContainer = document.getElementById("js-img");
-console.log(imgContainer);
+
 let clickCounter = 0;
 imgContainer.addEventListener("click", () => {
   clickCounter += 1;
-  //hola profe, aqui me la estoy jugando... solo busque como determinar si un numero es primo o no. no creo que sea algo que "no nos han pasado cierto?"
+  //hola profe, mantuve el codigo original para no hacer trampa
   if (clickCounter % 2 == 1) {
-    console.log(clickCounter);
     imgContainer.style.border = "2px solid red";
   } else {
     imgContainer.style.border = "";
@@ -21,21 +20,16 @@ const secondStickerQuantity = document.getElementById(
 const thirdStickerQuantity = document.getElementById("third-sticker-quantity");
 const btn = document.getElementById("calcButton");
 const result = document.getElementById("result");
-console.log(firstStickerQuantity);
-console.log(secondStickerQuantity);
-console.log(thirdStickerQuantity);
-console.log(btn);
-console.log(result);
+
 btn.addEventListener("click", () => {
   const a = firstStickerQuantity.value;
   const b = secondStickerQuantity.value;
   const c = thirdStickerQuantity.value;
   const sum = Number(a) + Number(b) + Number(c);
-  console.log(sum);
   if (sum <= 10) {
     result.innerHTML = "total: " + sum;
   } else {
-    result.innerHTML = "Llevas demasiados golosooo!";
+    result.innerHTML = "Llevas demasiados golosooo! tuki";
   }
 });
 //aqui termina el js del trabajo 2
@@ -45,12 +39,6 @@ const segundoDigito = document.getElementById("segundo-digito");
 const tercerDigito = document.getElementById("tercer-digito");
 const passBtn = document.getElementById("pass-button");
 const passResult = document.getElementById("pass-result");
-
-console.log(primerDigito.value);
-console.log(segundoDigito.value);
-console.log(tercerDigito.value);
-console.log(passBtn);
-console.log(passResult);
 
 passBtn.addEventListener("click", () => {
   const selectedPass =
